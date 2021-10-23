f5684ec77f620c19dfef94595b44783b70e2d5dc
akerlund <fredrik.akerlund.lh@outlook.com>
2021-10-23 10:21:52 +0200

    AXI4: Example's compile.sh

--- examples/vip_axi4_agent/scripts/compile.sh
+++ examples/vip_axi4_agent/scripts/compile.sh
@@ -8,6 +8,7 @@ uvm_top="axi4_tb_top"
 
 # Specify other file lists
 source $git_root/bool/files.lst
+source $git_root/vip_memory/files.lst
 source $git_root/vip_axi4_agent/files.lst
 source $git_root/vip_clk_rst_agent/files.lst
 source $git_root/report_server/files.lst
