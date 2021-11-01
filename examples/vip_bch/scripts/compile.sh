13f3d34628011fb0f86ad9cb63b6fa7aad203366
akerlund <fredrik.akerlund.lh@outlook.com>
2021-11-01 21:14:12 +0100

    BCH: Blank example

---
+++ examples/vip_bch/scripts/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git_root="$(git rev-parse --show-toplevel)"
+
+rtl_top="bch_tb_top"
+uvm_top="bch_tb_top"
+
+source $git_root/bool/files.lst
+#source $git_root/vip_bch/files.lst
+
+source ./tb/files.lst
